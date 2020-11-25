7cd269a246f54f357a4879402aee2c2b66819b7c
a-rfl <59601726+a-rfl@users.noreply.github.com>
2020-11-25 14:26:08 +0100

    Add seed parcel

---
+++ parcel-project/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'no-console': 'off',
+    'vars-on-top': 'off',
+    'func-names': 'off',
+    'no-restricted-syntax': 'off',
+    'no-plusplus': 'off',
+    'no-loop-func': 'off',
+  },
+};
