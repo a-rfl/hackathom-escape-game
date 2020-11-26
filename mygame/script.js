466eaa6746225eb741776a94e3a7c0470ed9cca2
a-rfl <59601726+a-rfl@users.noreply.github.com>
2020-11-26 15:16:25 +0100

    Delete parcel-seed

--- mygame/script.js
+++ mygame/script.js
@@ -32,25 +32,25 @@ $('.btn-play').on('click', () => {
   }
 });
 
-// const showEnigma = (objet) => {
-//   let popupEnigme = '<div class="enigme"><i class="far fa-times-circle"></i>';
-//   enigmes.forEach((enigme) => {
-//     if (enigme.objet === objet) {
-//       popupEnigme += `<p class="text-enigme">${enigme.enonce}</p>`;
-//       if (enigme.reponsesProposees.length === 0) {
-//         popupEnigme += '<input type="text" class="ipt-enigme"/>';
-//       } else {
-//         enigme.reponsesProposees.forEach((proposition) => {
-//           popupEnigme += `<div class="reponse-enigme ipt-enigme">${proposition}</div>`;
-//         });
-//       }
-//       popupEnigme += '<button class="btn-enigme">C\'est mon dernier mot</button></div>';
-//       document.body.innerHTML += popupEnigme;
-//     }
-//   });
-// };
-// const objet = document.getElementById('stallman');
+const showEnigma = (objet) => {
+  let popupEnigme = '<div class="enigme"><i class="far fa-times-circle"></i>';
+  enigmes.forEach((enigme) => {
+    if (enigme.objet === objet) {
+      popupEnigme += `<p class="text-enigme">${enigme.enonce}</p>`;
+      if (enigme.reponsesProposees.length === 0) {
+        popupEnigme += '<input type="text" class="ipt-enigme"/>';
+      } else {
+        enigme.reponsesProposees.forEach((proposition) => {
+          popupEnigme += `<div class="reponse-enigme ipt-enigme">${proposition}</div>`;
+        });
+      }
+      popupEnigme += '<button class="btn-enigme">C\'est mon dernier mot</button></div>';
+      document.body.innerHTML += popupEnigme;
+    }
+  });
+};
+const objet = document.getElementById('stallman');
 
-// objet.addEventListener('click', (e) => {
-//   showEnigma(e.currentTarget.id);
-// });
+objet.addEventListener('click', (e) => {
+  showEnigma(e.currentTarget.id);
+});
